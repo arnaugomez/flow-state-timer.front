957672e45294f42aebbaf1abd76206a917b06745
Arnau Gómez Farell <agomez1f@gmail.com>
2022-12-15 20:31:45 +0100

    feat: header

--- src/root.tsx
+++ src/root.tsx
@@ -18,17 +18,30 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Flow State Timer</Title>
+        <Meta
+          name="description"
+          content="Reach flow state with a pomodoro timer that learns from you."
+        />
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A class="mr-2" href="/">
-              Index
-            </A>
-            <A href="/about">About</A>
+            <div class="m-4 flex justify-between items-center">
+              <h1>Flow State Timer</h1>
+              <div class="flex items-center space-x-4">
+                <A href="/about">
+                  <span>?</span>
+                  <span class="sr-only">About</span>
+                </A>
+                <A href="/roadmap">
+                  <span>O</span>
+                  <span class="sr-only">Roadmap</span>
+                </A>
+              </div>
+            </div>
             <Routes>
               <FileRoutes />
             </Routes>
